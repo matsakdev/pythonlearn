95291c14f5cf8e9871e1a138ebdc68d6888379a0
Stanislav Matsak <stanislav.matsak@nure.ua>
2023-03-12 04:42:13 +0200

    Pz1: Create decoder

---
+++ pz1/__init__.py
@@ -0,0 +1,21 @@
+text = "vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf\"up."
+
+def decode(string, shift):
+    shifted_text = ""
+    for char in string:
+        char_code = ord(char)
+        shifted_char_code = char_code + shift
+        shifted_char = chr(shifted_char_code)
+        shifted_text += shifted_char
+    return shifted_text
+
+
+
+if __name__ == '__main__':
+    for i in range(-20, 20):
+        print('*****SHIFT: ' + str(i))
+        shifted_text = decode(text, i)
+        print(shifted_text)
+
+
+
