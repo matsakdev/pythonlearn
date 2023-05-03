b1b8de2a9c6968ea8b9bad4d1244ae4341918fba
Stanislav Matsak <stanislav.matsak@nure.ua>
2023-05-03 22:39:48 +0300

    Add schedule by week

--- lab3/__init__.py
+++ lab3/__init__.py
@@ -5,4 +5,5 @@ import task2
 
 if __name__ == '__main__':
     # task1.getAllFaculties()
-    task2.getScheduleForTheDay('ПЗПІ-20-6', datetime.datetime(2023, 3, 10))
+    task2.print_schedule_by_day('ПЗПІ-20-6', datetime.datetime(2023, 5, 4))
+    task2.print_schedule_by_week('ПЗПІ-20-6', datetime.datetime(2023, 5, 4))
--- lab3/task2.py
+++ lab3/task2.py
@@ -1,5 +1,5 @@
 import datetime
-from calendar import calendar
+import calendar
 import requests
 import constants
 
@@ -18,22 +18,27 @@ def find_group_id(group_name):
 
     for faculty in groups_data['university']['faculties']:
         for direction in faculty['directions']:
+            print('[direction]', direction)
             for speciality in direction['specialities']:
                 for group in speciality['groups']:
                     if group['name'] == group_name:
                         return group['id']
-            if direction['groups']:
+            if 'groups' in direction:
+                print('groups in direction')
                 for group in direction['groups']:
+                    print('[group]', group)
                     if group['name'] == group_name:
                         return group['id']
+
 
 def get_subject_by_id(id, subjects):
     for subject in subjects:
         if subject['id'] == id:
             return subject['brief']
-def getScheduleForTheDay(group_name, day):
+
+
+def print_schedule_by_day(group_name, day):
     group_id = find_group_id(group_name)
-    print(group_id)
 
     # Отримуємо JSON з розкладом групи за допомогою запиту GET
     response = requests.get(f'{BASE_API_URL}/P_API_EVEN_JSON?type_id=1&timetable_id={group_id}&idClient=KNURESked')
@@ -54,20 +59,34 @@ def getScheduleForTheDay(group_name, day):
             print(
                 f"{event['number_pair']} пара: {event['auditory']}, {name_subject}, {event_start_time.strftime('%H:%M')} - {event_end_time.strftime('%H:%M')}")
 
+
 #
-# # Функція для виведення розкладу за тиждень
-# def print_schedule_by_week(week):
-#     week_start = week - datetime.timedelta(days=week.weekday())
-#     week_end = week_start + datetime.timedelta(days=6)
-#     print('Розклад занять групи з', week_start.strftime('%d.%m.%Y'), 'по', week_end.strftime('%d.%m.%Y'))
-#     for event in group_events:
-#         event_start_time = timestamp_to_datetime(event['start_time'])
-#         event_end_time = timestamp_to_datetime(event['end_time'])
-#         if week_start.date() <= event_start_time.date() <= week_end.date():
-#             nameSubject = get_subject_by_id(event['subject_id'])
-#             print(
-#                 f"{event['number_pair']} пара: {event['auditory']}, {nameSubject}, {event_start_time.strftime('%d.%m.%Y %H:%M')} - {event_end_time.strftime('%H:%M')}")
-#
+# Функція для виведення розкладу за тиждень
+def print_schedule_by_week(group_name, week):
+    group_id = find_group_id(group_name)
+
+    week_start = week - datetime.timedelta(days=week.weekday())
+    week_end = week_start + datetime.timedelta(days=6)
+
+    # Отримуємо JSON з розкладом групи за допомогою запиту GET
+    response = requests.get(f'{BASE_API_URL}/P_API_EVEN_JSON?type_id=1&timetable_id={group_id}&idClient=KNURESked')
+
+    # Розпаковуємо JSON із відповіді
+    json_data = response.json()
+
+    # Знаходимо всі пари, що належать до групи
+    group_events = [event for event in json_data['events'] if group_id in event['groups']]
+    subjects = [subject for subject in json_data['subjects']]
+
+    print('Розклад занять групи на тиждень (', week_start.strftime('%d.%m.%Y'), ' - ', week_end.strftime('%d.%m.%Y'), ')')
+    for event in group_events:
+        event_start_time = timestamp_to_datetime(event['start_time'])
+        event_end_time = timestamp_to_datetime(event['end_time'])
+        if week_start.date() <= event_start_time.date() <= week_end.date():
+            name_subject = get_subject_by_id(event['subject_id'], subjects)
+            print(
+                f"{event['number_pair']} пара: {event['auditory']}, {nameSubject}, {event_start_time.strftime('%d.%m.%Y %H:%M')} - {event_end_time.strftime('%H:%M')}")
+
 #
 # # Функція для виведення розкладу за місяць
 # def print_schedule_by_month(month):
