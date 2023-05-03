3d941268b8c9108f86ed89da1d23fb2babf6f453
Stanislav Matsak <stanislav.matsak@nure.ua>
2023-05-03 22:53:47 +0300

    Add schedule for month

--- lab3/__init__.py
+++ lab3/__init__.py
@@ -4,6 +4,7 @@ import task1
 import task2
 
 if __name__ == '__main__':
-    task1.getAllFaculties()
-    task2.print_schedule_by_day('ПЗПІ-20-6', datetime.datetime(2023, 5, 4))
-    task2.print_schedule_by_week('ПЗПІ-20-6', datetime.datetime(2023, 5, 4))
+    # task1.getAllFaculties()
+    # task2.print_schedule_for_day('ПЗПІ-20-6', datetime.datetime(2023, 5, 4))
+    # task2.print_schedule_for_week('ПЗПІ-20-6', datetime.datetime(2023, 5, 4))
+    task2.print_schedule_for_month('ПЗПІ-20-6', datetime.datetime(2023, 5, 10))
--- lab3/task2.py
+++ lab3/task2.py
@@ -34,7 +34,7 @@ def get_subject_by_id(id, subjects):
             return subject['brief']
 
 
-def print_schedule_by_day(group_name, day):
+def print_schedule_for_day(group_name, day):
     group_id = find_group_id(group_name)
 
     # Отримуємо JSON з розкладом групи за допомогою запиту GET
@@ -59,7 +59,7 @@ def print_schedule_by_day(group_name, day):
 
 #
 # Функція для виведення розкладу за тиждень
-def print_schedule_by_week(group_name, week):
+def print_schedule_for_week(group_name, week):
     group_id = find_group_id(group_name)
 
     week_start = week - datetime.timedelta(days=week.weekday())
@@ -84,23 +84,29 @@ def print_schedule_by_week(group_name, week):
             print(
                 f"{event['number_pair']} пара: {event['auditory']}, {name_subject}, {event_start_time.strftime('%d.%m.%Y %H:%M')} - {event_end_time.strftime('%H:%M')}")
 
-#
-# # Функція для виведення розкладу за місяць
-# def print_schedule_by_month(month):
-#     month_start = datetime.datetime(month.year, month.month, 1)
-#     month_end = datetime.datetime(month.year, month.month, calendar.monthrange(month.year, month.month)[1])
-#     print('Розклад занять групи за', month_start.strftime('%B %Y'))
-#     for event in group_events:
-#         event_start_time = timestamp_to_datetime(event['start_time'])
-#         event_end_time = timestamp_to_datetime(event['end_time'])
-#         if month_start.date() <= event_start_time.date() <= month_end.date():
-#             nameSubject = get_subject_by_id(event['subject_id'])
-#             print(
-#                 f"{event['number_pair']} пара: {event['auditory']}, {nameSubject}, {event_start_time.strftime('%d.%m.%Y %H:%M')} - {event_end_time.strftime('%H:%M')}")
-#
-#
-# day = datetime.datetime(2023, 3, 10)
-# week = datetime.datetime(2023, 3, 13)
-# month = datetime.datetime(2023, 3, 1)
-# print_schedule_by_week(week)
-# print_schedule_by_month(month)
+
+# Функція для виведення розкладу за місяць
+def print_schedule_for_month(group_name, month):
+    group_id = find_group_id(group_name)
+
+    month_start = datetime.datetime(month.year, month.month, 1)
+    month_end = datetime.datetime(month.year, month.month, calendar.monthrange(month.year, month.month)[1])
+
+    # Отримуємо JSON з розкладом групи за допомогою запиту GET
+    response = requests.get(f'{BASE_API_URL}/P_API_EVEN_JSON?type_id=1&timetable_id={group_id}&idClient=KNURESked')
+
+    # Розпаковуємо JSON із відповіді
+    json_data = response.json()
+
+    # Знаходимо всі пари, що належать до групи
+    group_events = [event for event in json_data['events'] if group_id in event['groups']]
+    subjects = [subject for subject in json_data['subjects']]
+
+    print('Розклад занять групи на місяць (', month_start.strftime('%B %Y'), ')')
+    for event in group_events:
+        event_start_time = timestamp_to_datetime(event['start_time'])
+        event_end_time = timestamp_to_datetime(event['end_time'])
+        if month_start.date() <= event_start_time.date() <= month_end.date():
+            name_subject = get_subject_by_id(event['subject_id'], subjects)
+            print(
+                f"{event['number_pair']} пара: {event['auditory']}, {name_subject}, {event_start_time.strftime('%d.%m.%Y %H:%M')} - {event_end_time.strftime('%H:%M')}")
